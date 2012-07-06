1fff20b1fa82a4bda6e99883ae09bb60c7dc6255
Daniel M. Lofaro <dan@danlofaro.com>
2012-07-06 17:52:53 -0400

    added last plot for velocity of end effector now need to add intro and outtro

---
+++ fig/makePlot.m
@@ -0,0 +1,26 @@
+clear all
+close all
+load velos
+a = a';
+A = sqrt(sum(a.^2));
+A = A([1:278,300:length(A)]);
+T = 0.01;
+
+A = smooth(A,40);
+
+k = 4.8/max(A);
+
+A = A*k;
+
+L = length(A);
+t = 1:-1/(L-367):0;
+t = t';
+
+A = [A(1:366);A(367:L).*t];
+
+
+%plot((1:length(a))*T,A);
+plot((1:length(A))*T,A,'LineWidth',3);
+grid on
+axis([0 5.3, 0, 5]) 
+shg
